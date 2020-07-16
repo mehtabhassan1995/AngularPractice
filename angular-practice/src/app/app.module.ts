ebbcdca89d1abf02ba40a545e290417e3237386f
Syed Mehtab Hassan <mehtabhassan1995@gmail.com>
2020-07-16 09:36:49 -0700

    Routing in Angular

--- angular-practice/src/app/app.module.ts
+++ angular-practice/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
 // import { AppComponent } from './app.component';
 import { FirstcomponentComponent } from './firstcomponent/firstcomponent.component';
-import { DbcomponentComponent } from './dbcomponent/dbcomponent.component';
-import { PbcomponentComponent } from './pbcomponent/pbcomponent.component';
-import { EbcomponentComponent } from './ebcomponent/ebcomponent.component';
-import { InputcomponentComponent } from './inputcomponent/inputcomponent.component';
-import { DirectivecomponentComponent } from './directivecomponent/directivecomponent.component';
+import { DbcomponentComponent } from './Basic/dbcomponent/dbcomponent.component';
+import { PbcomponentComponent } from './Basic/pbcomponent/pbcomponent.component';
+import { EbcomponentComponent } from './Basic/ebcomponent/ebcomponent.component';
+import { InputcomponentComponent } from './Basic/inputcomponent/inputcomponent.component';
+import { DirectivecomponentComponent } from './Basic/directivecomponent/directivecomponent.component';
+import { UsercomponentComponent } from './Routing/usercomponent/usercomponent.component';
+import { HomecomponentComponent } from './Routing/homecomponent/homecomponent.component';
+import { UserListComponent } from './Routing/user-list/user-list.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomecomponentComponent},
+  { path: 'users/:id', component: UsercomponentComponent},
+  { path: 'users', component: UserListComponent}
+]
 
 @NgModule({
   declarations: [
@@ -18,11 +28,15 @@ import { DirectivecomponentComponent } from './directivecomponent/directivecompo
     PbcomponentComponent,
     EbcomponentComponent,
     InputcomponentComponent,
-    DirectivecomponentComponent
+    DirectivecomponentComponent,
+    UsercomponentComponent,
+    HomecomponentComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [FirstcomponentComponent]
